8f61c7042784e53e1d867252226809c1ca4e3fd0
Yyan-60306991 <60306991@udst.edu.qa>
2026-02-10 21:46:22 +0300

    Modified the main.js into the presentation.js, only contains the console.log/UI functionalities.

--- a2_files/presentation.js
+++ a2_files/presentation.js
@@ -6,7 +6,63 @@
  */
 
 const prompt = require('prompt-sync')()
+const fs = require('fs/promises')
 const bsn = require('./business')
+
+/**
+ * Prints the data from the function allEmployees.
+ * 
+ * @param {Array} data[] Takes an array as input.
+ * @returns {console.log} Prints out each value of the array.
+ */
+async function printEmployees(data){
+    if (data.length === 0) {
+        console.log(`========================`)
+        console.log(`The file has no records.`)
+        console.log(`========================`)
+    } else {
+        for (c of data) {
+            console.log(c)
+        }
+    }
+}
+
+/**
+ * Prints the schedule data of the employees.
+ * 
+ * @param {Array} data[] Takes an array as input.
+ * @returns {console.log} Prints out each value of the array.
+ */
+async function printSchedule(data){
+    if (data.length != 0){
+            console.log(`========================`)
+            console.log(`date, startTime, endTime`)
+            for (s of data){
+                console.log(s)
+            }
+            console.log(`========================`)
+    } else {
+        console.log(`=================================================`)
+        console.log(`The employee ID: ${employee} does not have a schedule.`)
+        console.log(`=================================================`)
+    }
+}
+
+/**
+ * Prints the assignments data of the employees.
+ * 
+ * @param {Array} data[] Takes an array as input.
+ * @returns {console.log} Prints out each value of the array.
+ */
+async function printAssigned(data){
+    if (data.length === 0){
+        console.log(``)
+    } else {
+        for (d of data){
+            console.log(d)
+        }
+    }
+}
 
 /**
  * Main interface of the app
@@ -22,19 +78,24 @@ async function main() {
         let choice = Number(prompt("What is your choice> "))
 
         if (choice == 1) {
-            await allEmployees()
+            data = await bsn.allEmployees()
+            await printEmployees(data)
         } else if (choice == 2) {
             let empName = prompt(`Enter employee name: `)
             let number = prompt(`Enter phone number: `)
-            await addEmployee(empName, number)
-            console.log(`Employee added...`)
+            data = await bsn.addEmployee(empName, number)
+
         } else if (choice == 3) {
             let empID = prompt("Enter employee ID: ").toUpperCase()
             let shiftID = prompt("Enter shift ID: ").toUpperCase()
-            await assignShift(empID, shiftID)
+            data = await bsn.assignShift(empID, shiftID)
+            await printAssigned(data)
+
         } else if (choice == 4) {
             let empID = prompt("Enter employee ID: ").toUpperCase()
-            await empSchedule(empID)
+            data = await bsn.empSchedule(empID)
+            await printSchedule(data)
+
         } else if (choice == 5) {
             break
         } else {
