7517c16f79ebef6d78124eb0d367bf653e3eab63
Yyan-60306991 <60306991@udst.edu.qa>
2026-02-10 21:44:56 +0300

    Added the business layer functions, only the request call from the persistence layer.

--- a2_files/business.js
+++ a2_files/business.js
@@ -5,5 +5,28 @@
  * Business Layer - Handles the business operations
  */
 
-const fs = require('fs/promises')
 const perst = require('./persistence')
+
+async function allEmployees(){
+    return await perst.allEmployees()
+}
+
+async function addEmployee(name, num){
+    return await perst.addEmployee(name, num)
+}
+
+async function assignShift(employee, shift){
+    
+    return await perst.assignShift(employee, shift)
+}
+
+async function empSchedule(employee){
+    return await perst.empSchedule(employee)
+}
+
+module.exports = {
+    allEmployees,
+    addEmployee,
+    assignShift,
+    empSchedule
+}
