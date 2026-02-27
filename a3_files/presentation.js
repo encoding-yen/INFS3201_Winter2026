509af96ce722c67b0412b4a9b12ff410c340f83c
Yyan-60306991 <60306991@udst.edu.qa>
2026-02-27 23:18:32 +0300

    Added the Employee Details page.

--- a3_files/presentation.js
+++ a3_files/presentation.js
@@ -12,16 +12,65 @@ const app = express()
 
 app.get('/', async (req, res) => {
     let emp = await bsn.allEmployees()
-    console.log(emp)
+    // console.log(emp)
     let data = ""
     for (e of emp){
-        data += `<li><a href="/employee/${e.id}>">${e.name}</li>`
+        data += `<li><a href="/employee/${e.employeeId}">${e.name}</li>`
     }
     
     res.send(`
         <h1>List of Employees</h1>
         <ul>${data}</ul>
         `)
+})
+
+app.get('/employee/:id', async(req, res) => {
+    let empId = req.params.id
+    let employee = await bsn.getOneEmployee(empId)
+    let schedule = await bsn.empSchedule(empId)
+    // console.log(empId, employee)
+
+    let result = `<tr><th>Date</th><th >Start Time</th><th>End Time</th></tr>`
+
+    if (schedule === undefined){
+        res.send(`
+            <style>
+                table tr:nth-child(3) td:nth-child(2) {
+                    background-color: yellow;
+                }
+            </style>
+            <h1>Employee Details</h1>
+            <p>Name: ${employee.name}</p>
+            <p>Phone: ${employee.phone}</p>
+            <a href="/edit/${employee.employeeId}>">Edit Details</a>
+
+            <h2>Shifts</h2>
+            <table>${result}</table>
+        `)
+    } else {
+        for (let i = 0; i < schedule.length; i++){
+            result += `<tr>
+                <td>${schedule[i].date}</td>
+                <td>${schedule[i].startTime}</td>
+                <td>${schedule[i].endTime}</td>
+            </tr>`
+        }
+
+        res.send(`
+            <style>
+                table tr:nth-child(3) td:nth-child(2) {
+                    background-color: yellow;
+                }
+            </style>
+            <h1>Employee Details</h1>
+            <p>Name: ${employee.name}</p>
+            <p>Phone: ${employee.phone}</p>
+            <a href="/edit/${employee.employeeId}>">Edit Details</a>
+
+            <h2>Shifts</h2>
+            <table>${result}</table>
+            `)
+        }
 })
 
 app.listen(8000)
