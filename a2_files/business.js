5ac5157e11ea27591f1f9874387bd9fb63e5d200
Yyan-60306991 <60306991@udst.edu.qa>
2026-02-28 04:56:48 +0300

    Resolved merging issue with a2_files

--- a2_files/business.js
+++ a2_files/business.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /**
  * Yyan Saguinsin
  * 60306991
@@ -28,4 +29,37 @@ module.exports = {
     addEmployee,
     assignShift,
     empSchedule
+=======
+/**
+ * Yyan Saguinsin
+ * 60306991
+ * 
+ * Business Layer - Handles the business operations
+ */
+
+const perst = require('./persistence')
+
+async function allEmployees(){
+    return await perst.allEmployees()
+}
+
+async function addEmployee(name, num){
+    return await perst.addEmployee(name, num)
+}
+
+async function assignShift(employee, shift){
+    
+    return await perst.assignShift(employee, shift)
+}
+
+async function empSchedule(employee){
+    return await perst.empSchedule(employee)
+}
+
+module.exports = {
+    allEmployees,
+    addEmployee,
+    assignShift,
+    empSchedule
+>>>>>>> 10e9596bd7a1886973374ca1ad3888220e9d8767
 }
