b69ff63e75571f154e63c0caf074c6fffa4ec9ab
Yyan-60306991 <60306991@udst.edu.qa>
2026-02-28 04:37:27 +0300

    Added the form page and server validation.

--- a3_files/presentation.js
+++ a3_files/presentation.js
@@ -42,11 +42,12 @@ app.get('/employee/:id', async(req, res) => {
             <h1>Employee Details</h1>
             <p>Name: ${employee.name}</p>
             <p>Phone: ${employee.phone}</p>
-            <a href="/edit/${employee.employeeId}>">Edit Details</a>
+            <a href="/edit/${employee.employeeId}">Edit Details</a>
 
             <h2>Shifts</h2>
             <table>${result}</table>
         `)
+
     } else {
         for (let i = 0; i < schedule.length; i++){
             result += `<tr>
@@ -65,12 +66,60 @@ app.get('/employee/:id', async(req, res) => {
             <h1>Employee Details</h1>
             <p>Name: ${employee.name}</p>
             <p>Phone: ${employee.phone}</p>
-            <a href="/edit/${employee.employeeId}>">Edit Details</a>
+            <a href="/edit/${employee.employeeId}">Edit Details</a>
 
             <h2>Shifts</h2>
             <table>${result}</table>
             `)
         }
+})
+
+app.get('/edit/:id', async(req, res) => {
+    let empId = req.params.id
+    let employee = await bsn.getOneEmployee(empId)
+    
+    // console.log(empId)
+
+    res.send(`
+        <h1>Edit</h1>
+        <form method='POST' action='/edit/${employee.employeeId}'>
+            <label>Name: <input type="text" name="name" value="${employee.name}"></label><br>
+            <label>Phone: <input type="text" name="phone" value="${employee.phone}"></label><br>
+            <button type="submit">Save</button>
+        </form>
+        `)
+})
+
+app.use(express.urlencoded({ extended: true }))
+
+app.post('/edit/:id', async(req, res) => {
+    let empId = req.params.id
+    let name = req.body.name.trim()
+    let number = req.body.phone.trim()
+
+    if (name === '' || name === null){
+        return res.send(`The field name can not be empty`)
+    }
+
+    if (number.length !== 9 || number[4] !== '-') {
+        return res.send('Phone must be in format XXXX-XXXX')
+    }
+
+    for (let i = 0; i < 4; i++) {
+        if (number[i] < '0' || number[i] > '9') {
+            return res.send('Phone must be in format XXXX-XXXX')
+        }
+    }
+
+    for (let i = 5; i < 9; i++) {
+        if (number[i] < '0' || number[i] > '9') {
+            return res.send('Phone must be in format XXXX-XXXX')
+        }
+    }
+
+    await bsn.editEmployee(empId, name, number)
+
+    res.redirect('/')
 })
 
 app.listen(8000)
