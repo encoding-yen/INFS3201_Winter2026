86f80fd987cdf27ac001e0bba1c22367fb720915
Yyan-60306991 <60306991@udst.edu.qa>
2026-02-28 04:39:23 +0300

    Added all the necessary functions.

--- a3_files/persistence.js
+++ a3_files/persistence.js
@@ -5,90 +5,53 @@
  * Persistence Layer - Handles the file operations
  */
 
-const { checkPrime } = require('crypto')
-const { read } = require('fs')
 const fs = require('fs/promises')
+const mongodb = require('mongodb')
+
+let client = undefined
 
 /**
- * Parses a number which the function opens and reads the content to be used.
+ * Reads the database from the given cluster.
  * 
- * @param {String} file Opens and reads the content of the given file.
- * @returns {Array} Returns the content read from the file and turns it into a JavaScript readable array.
+ * @async
+ * @function readEmployeeData
+ * @returns {Promise<Array<Object>>} Provides the connection to the database.
  */
-async function readEmployeeData(file) {
-    if (file == 1) {
-        let raw = await fs.readFile('employees.json', 'utf8')
-        return await JSON.parse(raw)
-    } else if (file == 2) {
-        let raw = await fs.readFile('shifts.json', 'utf8')
-        return await JSON.parse(raw)
-    } else if (file == 3) {
-        let raw = await fs.readFile('assignments.json', 'utf8')
-        return await JSON.parse(raw)
-    } else if (file == 4){
-        let raw = await fs.readFile('config.json', 'utf8')
-        return await JSON.parse(raw)
-    } else {
-        console.log('There was no file passed to read.')
-    }
-}
-
-/**
- * Writes the strigified array onto an external file.
- * 
- * @param {Array} array[] takes the array and strigify it
- */
-async function writeEmployeeData(file, array) {
-    let newArr = JSON.stringify(array, null, 2)
-    await fs.writeFile(file, newArr)
+async function readEmployeeData() {
+    client = new mongodb.MongoClient('mongodb+srv://readOnly:12class34@cluster0.xhwpjs2.mongodb.net/')
+    await client.connect()
 }
 
 /**
  * Displays all the employees from the .txt file
- * NO parameters are needed to parse.
  * 
  * @returns 
  */
 async function allEmployees() {
-    let empData = await readEmployeeData(1)
-    let data = []
-    if (empData.length === 0) {
-        data.push(`The file has no records.`)
-    } else {
-        data.push(`=============================================`)
-        data.push(`Employee ID\tName\t\tPhone`)
-        data.push(`-----------\t-------------\t-------------`)
-        for (c of empData) {
-            data.push(`${c.employeeId.padEnd(16)}${c.name.padEnd(20)}${c.phone}`)
-        }
-        data.push(`=============================================`)
-    }
-    return data
+    await readEmployeeData()
+    let db = client.db('infs3201_winter2026')
+    let employees = db.collection('employees')
+    let result = employees.find()
+    // console.log(result)
+    return result.toArray()
 }
 
 /**
- * Appends a new employee with a new ID.
+ * Returns the matching employee.
  * 
- * @param {String} name Takes the employee name as a string.
- * @param {String} num Takes the phone number as a string.
+ * @async
+ * @function getOneEmployee
+ * @param {id} id Takes a string as the input.
+ * @returns {Promise<Array<Object>>} Returns only the matching employee.
  */
-async function addEmployee(name, num) {
-    let empData = await readEmployeeData(1)
-    let newID = 'E'
-
-    if (empData.length < 10) {
-        newID += '00' + String(empData.length + 1)
-    } else if (empData.length < 99) {
-        newID += '0' + String(empData.length + 1)
-    } else {
-        newID += String(empData.length + 1)
-    }
-
-    let newObj = { "employeeId": newID, "name": name, "phone": num }
-    empData.push(newObj)
-    await writeEmployeeData("employees.json", empData)
+async function getOneEmployee(id){
+    await readEmployeeData()
+    let db = client.db('infs3201_winter2026')
+    let employees = db.collection('employees')
+    let result = await employees.findOne({ employeeId: id })
+    console.log(result)
+    return result
 }
-
 
 /**
  * Prints out the schedule of the given employee ID.
@@ -96,26 +59,48 @@ async function addEmployee(name, num) {
  * @param {String} employee Takes the employee ID as a string.
  */
 async function empSchedule(employee) {
-    let shiftID = await readEmployeeData(2)
-    let assignID = await readEmployeeData(3)
-    let schedule = []
+    await readEmployeeData()
+    let db = client.db('infs3201_winter2026')
+    let assign = db.collection('assignments')
+    let shifts = await assign.find({ employeeId: employee }).toArray()
 
-    for (i of assignID) {
-        if (i['employeeId'] == employee) {
-            let schedID = i['shiftId']
-            for (c of shiftID) {
-                if (c['shiftId'] == schedID) {
-                    schedule.push(`${c['date']}, ${c['startTime']}, ${c['endTime']}`)
-                }
-            }
-        }
+    if (shifts.length === 0) {
+        return undefined
     }
-    return schedule
+
+    let sId = []
+    for (let i = 0; i < shifts.length; i++) {
+        sId.push(shifts[i].shiftId)
+    }
+
+    let schedule = db.collection('shifts')
+    let result = await schedule.find({ shiftId: {$in: sId }}).toArray()
+    // console.log(result)
+
+    return result
+}
+
+/**
+ * Appends a new employee with a new ID.
+ * 
+ * @async
+ * @function editEmployee
+ * @param {String} name Takes the employee name as a string.
+ * @param {String} num Takes the phone number as a string.
+ * @returns {}
+ */
+async function editEmployee(empId, empName, empNum) {
+    await readEmployeeData()
+    let db = client.db('infs3201_winter2026')
+    let edited = db.collection('employees')
+    let result = await edited.updateOne({ employeeId: empId },{ $set: { name: empName, phone: empNum } })
+    return
 }
 
 module.exports = {
     readEmployeeData,
     allEmployees,
-    addEmployee,
+    getOneEmployee,
+    editEmployee,
     empSchedule,
 }
